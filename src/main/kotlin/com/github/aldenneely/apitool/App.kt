4098eaadbd4fd78347e1178382a7c69588c1fa1f
Alden Neely <alden.s.neel@gmail.com>
2017-05-15 13:51:42 -0500

    Querying

--- src/main/kotlin/com/github/aldenneely/apitool/App.kt
+++ src/main/kotlin/com/github/aldenneely/apitool/App.kt
@@ -2,10 +2,64 @@ package com.github.aldenneely.apitool
 
 import com.xenomachina.argparser.ArgParser
 
+import com.github.kittinunf.fuel.*
+import com.github.kittinunf.result.Result
+
+import kotlin.system.exitProcess
+import com.jayway.jsonpath.*
+
 fun main(args: Array<String>) {
     val parser = ArgParser(args)
     val parsedArgs = AppArgs(parser)
 
+    val app = App(parsedArgs, AppOutput())
+    app.run()
+}
+
+class AppOutput{
+
+    fun print(out: Any){
+        println(out)
+    }
+
+    fun fatal(out: Any){
+        println("FATAL ERROR: $out")
+        exitProcess(1)
+    }
+}
+
+class App(val args: AppArgs, val output: AppOutput) {
+
+    val GITHUB_URL_HEAD = "http://api.github.com"
+
+    fun run(){
+        makeApiRequest(args.request)
+    }
+
+    private fun parseJson(jsonSrc: String) {
+        val json: List<String> = JsonPath.read(jsonSrc, args.query)
+        output.print(json)
+    }
+
+    private fun makeApiRequest(requestUrl: String) {
+
+        val url = GITHUB_URL_HEAD + requestUrl
+
+        url.httpGet().responseString { _, _, result ->
+
+            when(result){
+                is Result.Failure -> {
+                    output.fatal("Could not perform request")
+                }
+
+                is Result.Success -> {
+                    val resp = result.get()
+                    parseJson(resp)
+                }
+            }
+
+        }
+    }
 }
 
 
